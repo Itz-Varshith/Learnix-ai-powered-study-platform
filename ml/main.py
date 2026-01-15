e0a3def76b92fc9f441925a3a0c35942882e3e26
S Varshith Reddy <svarshithreddy9121@gmail.com>
2026-01-15 19:58:06 +0530

    Minor changes in config

--- ml/main.py
+++ ml/main.py
@@ -19,10 +19,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:8000/summarize",
-        "http://127.0.0.1:8000"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
