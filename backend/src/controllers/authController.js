1747129ca870b123ead929994ccf0f1f73b8051a
S Varshith Reddy <svarshithreddy9121@gmail.com>
2026-01-15 22:39:43 +0530

    Changes in prisma generate pattern

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,9 +1,9 @@
-import { PrismaClient } from "../../generated/prisma/client.ts";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const loginUser = async (req, res) => {
   // If request reaches here, middleware has already verified the user
-  const { uid, email,name } = req.user;
+  const { uid, email, name } = req.user;
   await prisma.user.upsert({
     where: { id: uid },
     update: {
@@ -14,12 +14,12 @@ export const loginUser = async (req, res) => {
       id: uid,
       email: email,
       name: name,
-    }
-  })
+    },
+  });
   console.log(`User logged in: ${email}`);
-  return res.status(200).json({ 
-    message: "User is authenticated", 
+  return res.status(200).json({
+    message: "User is authenticated",
     uid,
-    email 
+    email,
   });
 };
--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../generated/prisma/client.ts";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getEnrolledCourses = async (req, res) => {
