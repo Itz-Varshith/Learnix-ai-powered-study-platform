9ba869bdd6b685058197a525b97bee4c0df3bb55
S Varshith Reddy <svarshithreddy9121@gmail.com>
2026-01-17 17:48:21 +0530

    Completed this version deploymeny

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ import { auth } from "../config/firebase.js";
 
 export const verifyToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-
+  
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ error: "No token provided" });
   }
